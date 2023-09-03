6c9a27a45bfcd7f5edfb7076b80b2143c4946d86
Per Næss <perness3009@gmail.com>
2023-09-03 08:59:26 +0200

    feat: secure user data by only allowing them to access their own resources

--- src/main/java/com/linkc/linkcbackend/config/WebSecurityConfig.java
+++ src/main/java/com/linkc/linkcbackend/config/WebSecurityConfig.java
@@ -1,14 +1,18 @@
 package com.linkc.linkcbackend.config;
 
+import com.linkc.linkcbackend.domain.User;
+import com.linkc.linkcbackend.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -24,11 +28,12 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        WebExpressionAuthorizationManager authorization = new WebExpressionAuthorizationManager("@webSecurity.checkUserId(authentication,#userId)");
+        WebSecurity webSecurity = new WebSecurity();
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/user/{userId}").access((authentication, context) -> new AuthorizationDecision(webSecurity.checkUserId(authentication.get(), context.getRequest())))
                         .anyRequest().authenticated())
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -36,5 +41,12 @@ public class WebSecurityConfig {
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    public static class WebSecurity {
+        public boolean checkUserId(Authentication authentication, HttpServletRequest request) {
+            User user = (User)authentication.getPrincipal();
+            return request.getServletPath().endsWith(user.getId()) && authentication.isAuthenticated();
+        }
     }
 }
