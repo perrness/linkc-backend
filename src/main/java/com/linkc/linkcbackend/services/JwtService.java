1cb2fa439023d0d624083722d4a581c80487d013
Per Næss <perness3009@gmail.com>
2023-09-03 12:11:16 +0200

    feat: add secret key as environment variable

--- src/main/java/com/linkc/linkcbackend/services/JwtService.java
+++ src/main/java/com/linkc/linkcbackend/services/JwtService.java
@@ -5,7 +5,9 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +20,14 @@ import java.util.function.Function;
 @Service
 public class JwtService {
     @Value("${SECRET_KEY}")
-    private static String SECRET_KEY = "";
+    private String key;
+
+    private static String SECRET_KEY;
+
+    @Value("${SECRET_KEY}")
+    public void setSecretKeyStatic(String key) {
+        JwtService.SECRET_KEY = key;
+    }
 
     public String extractUsername(String token) {
         return extractClaim(token, (Claims::getSubject));
