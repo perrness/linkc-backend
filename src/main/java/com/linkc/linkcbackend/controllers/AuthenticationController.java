dac9fbc86829cdcd899a3b95de1f4f419f0fd4a6
Per Næss <perness3009@gmail.com>
2023-09-04 17:23:16 +0200

    feat: better errorhandling for register endpoint

--- src/main/java/com/linkc/linkcbackend/controllers/AuthenticationController.java
+++ src/main/java/com/linkc/linkcbackend/controllers/AuthenticationController.java
@@ -3,7 +3,10 @@ package com.linkc.linkcbackend.controllers;
 import com.linkc.linkcbackend.domain.AuthenticationRequest;
 import com.linkc.linkcbackend.domain.AuthenticationResponse;
 import com.linkc.linkcbackend.domain.RegisterRequest;
+import com.linkc.linkcbackend.domain.Response;
 import com.linkc.linkcbackend.services.AuthenticationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthenticationController {
+    Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
+
     private final AuthenticationService authenticationService;
 
     public AuthenticationController(AuthenticationService authenticationService) {
@@ -20,8 +25,19 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authenticationService.register(request));
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        logger.info("Register new account");
+
+        try {
+            AuthenticationResponse authenticationResponse = authenticationService.register(request);
+            return ResponseEntity.ok(authenticationResponse);
+        } catch (Exception exception) {
+            logger.error("Register failed with: {}", exception.getMessage());
+            if (exception.getMessage().contains("E11000 duplicate key error collection: linkc.users. Failed _id or unique index constraint.")) {
+                return ResponseEntity.badRequest().body(new Response("Email or number already in use."));
+            }
+            return ResponseEntity.internalServerError().body("Something failed during registration");
+        }
     }
 
     @PostMapping("/authenticate")
